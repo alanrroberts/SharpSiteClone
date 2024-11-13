b3fafa43cab8f48a52b9aff0fa32e0204be60a98
Alan Roberts <alan@statistixl.com>
2024-11-13 20:23:01 +0000

    changes

---
+++ SharpSiteClone.AppHost/PostgresExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace SharpSiteClone.AppHost;
+
+public static class PostgresExtensions
+{
+    public static (IResourceBuilder<PostgresDatabaseResource> db, IResourceBuilder<ProjectResource> migrationSvc) AddPostgresServices(
+        this IDistributedApplicationBuilder builder,
+        string dbName)
+    {
+        var dbServer = builder.AddPostgres("database")
+            .WithDataVolume($"{dbName}-data")
+            .WithPgAdmin(c => c.WithLifetime(ContainerLifetime.Persistent))
+            .WithLifetime(ContainerLifetime.Persistent);
+
+        // create the db in postgres
+        var db = dbServer.AddDatabase(dbName);
+
+        var migrationSvc = builder.AddProject<Projects.SharpSiteClone_Data_Postgres_Migration>($"{dbName}-migrationsvc")
+            .WithReference(db)
+            .WaitFor(dbServer);
+
+        return (db, migrationSvc);
+    }
+}
--- SharpSiteClone.AppHost/Program.cs
+++ SharpSiteClone.AppHost/Program.cs
@@ -1,16 +1,9 @@
+using SharpSiteClone.AppHost;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 // aspire code to add docker containers for postgres and pgadmin
-var dbServer = builder.AddPostgres("database")
-    .WithPgAdmin(c => c.WithLifetime(ContainerLifetime.Persistent))
-    .WithLifetime(ContainerLifetime.Persistent);
-
-// create the db in postgres
-var db = dbServer.AddDatabase(SharpSiteClone.Data.Postgres.Constants.DBNAME);
-
-var migrationSvc = builder.AddProject<Projects.SharpSiteClone_Data_Postgres_Migration>("migrationsvc")
-    .WithReference(db)
-    .WaitFor(dbServer);
+var (db, migrationSvc) = builder.AddPostgresServices(SharpSiteClone.Data.Postgres.Constants.DBNAME);
 
 builder.AddProject<Projects.SharpSiteClone_Web>("webfrontend")
     .WithReference(db)
